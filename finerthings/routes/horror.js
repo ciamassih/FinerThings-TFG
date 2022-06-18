df6bb47f3afb456490afae1c11dbeeeeccbf313e
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-06-18 20:43:36 +0200

    Categorias funcionando

---
+++ finerthings/routes/horror.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Libro = require("../models/libros");
+const passport = require('passport');
+const User = require("../models/user")
+const ObjectId = require("mongoose").Types.ObjectId
+
+router.route('/')
+    .get(async function (req, res, next) {
+        let libros = await Libro.find({category: "Horror"})
+        console.log(req.user.favoritos)
+        res.render('horror.njk', {libros: libros});
+
+    });
+
+router.route('/:id')
+    .get(function(req, res, next){
+        Libro.findById(req.params.id, function (err, libro){
+            if (err) return console.log(err);
+            res.render('libro_detailed.njk', {libro: libro});
+        } )
+
+    });
+
+module.exports = router;
